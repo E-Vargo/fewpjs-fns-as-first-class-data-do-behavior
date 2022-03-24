231dfd32c430a862135190ec1c2925efa062b2e4
E-Vargo <vargoelliott@gmail.com>
2022-03-24 11:46:40 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(string) {
+const b = parseInt(string);
+
+  if ( b < 12) {
+  return 'Good Morning'
+  } if (17 > b  && b > 12) {
+  return 'Good Afternoon'
+  } else {
+  return 'Good Evening'
+  }
+
+}
 /* Write your implementation of displayMessage() */
+
+function displayMessage(arg){
+  document.getElementById('greeting').textContent = arg;
+}
